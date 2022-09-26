4f1349f14081f00574d0ca3fd06f961e775dd009
saeed ranjbar <saeed.rb71@gmail.com>
2022-09-26 15:32:28 +0330

    mapStateToProps training

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
+import ItemContainer from './components/ItemContainer';
 import NewCakeContainer from './components/NewCakeContainer';
 
 function App() {
   return (
     <div className="App">
+      <ItemContainer cake />
+      <ItemContainer />
       <HooksCakeContainer />
       <CakeContainer />
       <IceCreamContainer />
